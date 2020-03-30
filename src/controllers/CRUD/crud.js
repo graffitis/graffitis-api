15bf7e9698d816500d48f1858908658d58e1686b
Pietro Jomini <pietro.jomini@protonmail.ch>
2020-03-30 18:55:00 +0200

    crud patch & delete

--- src/controllers/CRUD/crud.js
+++ src/controllers/CRUD/crud.js
@@ -2,6 +2,10 @@ const get = (Model, schema) => async (req, res) => {
   const { id } = req.query;
 
   const query = id !== undefined ? { _id: id } : {};
+  schema.forEach((key) => {
+    if (req.query[key]) query[key] = req.query[key];
+  });
+
   const payload = { query, status: 200 };
 
   await Model.find(query)
@@ -9,24 +13,23 @@ const get = (Model, schema) => async (req, res) => {
       payload.err = err;
       payload.status = 500;
     })
-    .then((post) => {
-      payload.data = post;
+    .then((data) => {
+      payload.data = data;
     });
 
   res.status(payload.status).json(payload);
 };
 
 const post = (Model, schema) => async (req, res) => {
-  const payload = { status: 201 };
-
-  const query = {};
+  const body = {};
   schema.forEach((key) => {
-    query[key] = req.body[key];
+    if (req.body[key]) body[key] = req.body[key];
   });
-  query.edited = Date.now();
-  query.created = Date.now();
+  body.edited = Date.now();
+  body.created = Date.now();
 
-  const document = new Model(query);
+  const payload = { status: 201 };
+  const document = new Model(body);
 
   await document
     .save()
@@ -41,11 +44,59 @@ const post = (Model, schema) => async (req, res) => {
   res.status(payload.status).json(payload);
 };
 
+const patch = (Model, schema) => async (req, res) => {
+  const { id } = req.query;
+  const query = id ? { _id: id } : {};
+
+  const body = {};
+  schema.forEach((key) => {
+    if (req.body[key]) body[key] = req.body[key];
+  });
+  body.edited = Date.now();
+
+  const payload = { status: 200, query, body };
+
+  await Model.updateMany(query, body)
+    .catch((err) => {
+      payload.status = 500;
+      payload.err = err;
+    })
+    .then((data) => {
+      payload.data = data.nModified;
+    });
+
+  res.status(payload.status).json(payload);
+};
+
+const _delete = (Model, schema) => async (req, res) => {
+  const { id } = req.query;
+
+  const query = id !== undefined ? { _id: id } : {};
+  schema.forEach((key) => {
+    if (req.query[key]) query[key] = req.query[key];
+  });
+
+  const payload = { query, status: 200 };
+
+  await Model.deleteMany(query)
+    .catch((err) => {
+      payload.err = err;
+      payload.status = 500;
+    })
+    .then((data) => {
+      payload.data = data.deletedCount;
+    });
+
+  res.status(payload.status).json(payload);
+};
+
 const Crud = (Model) => {
   const schema = Object.keys(Model.schema.paths);
   return {
     get: get(Model, schema),
     post: post(Model, schema),
+    patch: patch(Model, schema),
+    delete: _delete(Model, schema),
   };
 };
 
